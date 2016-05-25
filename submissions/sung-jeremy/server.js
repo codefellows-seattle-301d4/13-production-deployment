fc8cea7e7fcfd6cb6a39a20db9d4d4c1a66b090a
Jeremy Perez <perezje43@gmail.com>
2016-05-25 14:45:00 -0700

    goodbye

--- submissions/sung-jeremy/server.js
+++ submissions/sung-jeremy/server.js
@@ -13,11 +13,11 @@ var proxyGitHub = function(request, response) {
 
 app.get('/github/*', proxyGitHub);
 
-app.use(express.static('./'));
+app.use(express.static('./submissions/sung-jeremy/'));
 
 app.get('*', function(request, response) {
   console.log('New request:', request.url);
-  response.sendFile('index.html', { root: 'submissions/sung-jeremy' });
+  response.sendFile('index.html', { root: '.' });
 });
 
 app.listen(port, function() {
